e7369371b36fc41a35c703c4ddbcaa01536759ef
Yo <yohanndore48@gmail.com>
2017-03-09 13:33:13 +0100

    tentative de mis en place de la barre de pagination

--- static/js/script.js
+++ static/js/script.js
@@ -9,15 +9,22 @@ $(document).ready(function() {
     var section = $("#produit")
 
     for (var i in catalog) {
+        // for (var j = 0; j < pictures.length; j++) {
+        //
+        // }
         console.log(catalog[0]);
-        var article = $("<article class=col-md-4></article>")
-        var nom = $("<h3>"+catalog[i].name+"</h3>");
-        var descr = $("<p>"+catalog[i].description+"</p>")
-        var prix = $("<p>"+catalog[i].price+"</p>")
-        var quant = $("<input type=number value="+catalog[i].quantity+">")
-        var vignette = $("<img src="+catalog[i].thumb+">")
-        var pict = $("<img src="+catalog[i].pictures+" class='img-responsive'>")
-        article.append(nom, descr, prix, quant, vignette, pict)
+        var article = $("<article class=col-md-3 item></article>")
+        var vignette = $("<img src=" + catalog[i].thumb + ">")
+        var nom = $("<h3>" + catalog[i].name + "</h3>");
+        var descr = $("<p>" + catalog[i].description + "</p>")
+        var prix = $("<p>" + "prix : " + catalog[i].price + "€" + "</p>")
+        var quant = $("<input type=number value=" + catalog[i].quantity + ">")
+        // var pict = $("<img src="+catalog[i].pictures+" class='img-responsive'>")
+
+
+        article.append(nom, vignette, descr, prix, quant)
         section.append(article)
     }
+
+
 });
