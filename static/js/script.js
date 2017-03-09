44c8543d342c28f542885892bef43539246c4f52
quentin <zhor.osu@gmail.com>
2017-03-09 11:04:28 +0100

    fin ajout objet

--- static/js/script.js
+++ static/js/script.js
@@ -5,4 +5,19 @@ $(document).ready(function() {
         cellAlign: 'left',
         contain: true
     });
+
+    var section = $("#produit")
+
+    for (var i in catalog) {
+        console.log(catalog[0]);
+        var article = $("<article class=col-md-4></article>")
+        var nom = $("<h3>"+catalog[i].name+"</h3>");
+        var descr = $("<p>"+catalog[i].description+"</p>")
+        var prix = $("<p>"+catalog[i].price+"</p>")
+        var quant = $("<input type=number value="+catalog[i].quantity+">")
+        var vignette = $("<img src="+catalog[i].thumb+">")
+        var pict = $("<img src="+catalog[i].pictures+" class='img-responsive'>")
+        article.append(nom, descr, prix, quant, vignette, pict)
+        section.append(article)
+    }
 });
